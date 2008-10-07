f41df1827adea3230b8145ba9014ca53c893cf17
Bjoern Hempel <info@hempel-online.de>
2008-10-07 14:35:51 +0000

    added basic ethernet functionallity (HTTP / PING) for unit testing based on http://avr-uip.googlecode.com/

--- culfw/Devices/DCUN/DCUN.c
+++ culfw/Devices/DCUN/DCUN.c
@@ -25,6 +25,14 @@
 #include "transceiver.h"
 #include "ttydata.h"
 
+#include "global-conf.h"
+#include "uip_arp.h"
+#include "network.h"
+#include "enc28j60.h"
+
+#include "timer.h"
+
+TASK(Ethernet_Task);
 
 /* Scheduler Task List */
 TASK_LIST
@@ -32,7 +40,8 @@ TASK_LIST
   { Task: USB_USBTask  , TaskStatus: TASK_STOP },
   { Task: CDC_Task     , TaskStatus: TASK_STOP },
   { Task: RfAnalyze_Task,TaskStatus: TASK_RUN },
-  { Task: Minute_Task,   TaskStatus: TASK_RUN },
+//  { Task: Minute_Task,   TaskStatus: TASK_STOP },
+  { Task: Ethernet_Task,   TaskStatus: TASK_RUN },
 };
 
 t_fntab fntab[] = {
@@ -75,15 +84,15 @@ EVENT_HANDLER(USB_Connect) {
 }
 
 EVENT_HANDLER(USB_Disconnect) {
-     PORTE |= _BV( PE1 );
      Scheduler_SetTaskMode(USB_USBTask, TASK_STOP);
      Scheduler_SetTaskMode(CDC_Task,    TASK_STOP);
      ccStrobe(CC1100_SIDLE);
 }
 
-int
-main(void)
-{
+#define BUF ((struct uip_eth_hdr *)&uip_buf[0])
+struct timer periodic_timer, arp_timer;
+
+int main(void) {
 
   // if we had been restarted by watchdog check the REQ BootLoader byte in the
   // EEPROM ...
@@ -94,10 +103,14 @@ main(void)
 
   MCUSR &= ~(1 << WDRF);
 
+  // reset Ethernet
+  DDRB   |= _BV( PB4 );
+  PORTB  &= ~_BV( PB4 );
+
   DDRE  |= _BV( PE1 );
   PORTE |= _BV( PE1 );
 
-  wdt_enable(WDTO_2S); 
+//  wdt_enable(WDTO_2S); 
 
   SetSystemClockPrescaler(0);                   // Disable Clock Division
 
@@ -113,10 +126,88 @@ main(void)
   TCCR0A = _BV(WGM01);
   TIMSK0 = _BV(OCIE0A);
 
+
+  // Network init
+  PORTB |= _BV( PB4 ); // unreset
+  network_init();
+
+  uip_ipaddr_t ipaddr;
+
+  timer_set(&periodic_timer, CLOCK_SECOND / 2);
+  timer_set(&arp_timer, CLOCK_SECOND * 10);
+
+  uip_init();
+
+  struct uip_eth_addr mac = {UIP_ETHADDR0, UIP_ETHADDR1, UIP_ETHADDR2, UIP_ETHADDR3, UIP_ETHADDR4, UIP_ETHADDR5};
+
+  uip_setethaddr(mac);
+  simple_httpd_init();
+
+#ifdef __DHCPC_H__
+  dhcpc_init(&mac, 6);
+#else
+  uip_ipaddr(ipaddr, 10,10,12,123);
+  uip_sethostaddr(ipaddr);
+  uip_ipaddr(ipaddr, 10,10,12,1);
+  uip_setdraddr(ipaddr);
+  uip_ipaddr(ipaddr, 255,255,255,0);
+  uip_setnetmask(ipaddr);
+#endif /*__DHCPC_H__*/
+
   Scheduler_Init();                            
 
   USB_Init();
 
   Scheduler_Start();                            // Won't return
+}
+
+TASK(Ethernet_Task) {
+     int i;
+     
+     uip_len = network_read();
+
+     if(uip_len > 0) {
+	  if(BUF->type == htons(UIP_ETHTYPE_IP)){
+	       uip_arp_ipin();
+	       uip_input();
+	       if(uip_len > 0) {
+		    uip_arp_out();
+		    network_send();
+	       }
+	  }else if(BUF->type == htons(UIP_ETHTYPE_ARP)){
+	       uip_arp_arpin();
+	       if(uip_len > 0){
+		    network_send();
+	       }
+	  }
+	  
+     }else if(timer_expired(&periodic_timer)) {
+	  timer_reset(&periodic_timer);
+	  
+	  for(i = 0; i < UIP_CONNS; i++) {
+	       uip_periodic(i);
+	       if(uip_len > 0) {
+		    uip_arp_out();
+		    network_send();
+	       }
+	  }
+	  
+#if UIP_UDP
+	  for(i = 0; i < UIP_UDP_CONNS; i++) {
+	       uip_udp_periodic(i);
+	       if(uip_len > 0) {
+		    uip_arp_out();
+		    network_send();
+	       }
+	  }
+#endif /* UIP_UDP */
+	  
+	  if(timer_expired(&arp_timer)) {
+	       PORTE  ^= _BV( PE1 );
+	       timer_reset(&arp_timer);
+	       uip_arp_timer();
+	  }
+     }
+     
 }
 
---
+++ culfw/Devices/DCUN/clock-arch.h
@@ -0,0 +1,7 @@
+#ifndef __CLOCK_ARCH_H__
+#define __CLOCK_ARCH_H__
+
+// all done in clib/clock.h
+#include "clock.h"
+
+#endif /* __CLOCK_ARCH_H__ */
