b30f4c951a506443e68a27c6f932ee856597b22c
Bjoern Hempel <info@hempel-online.de>
2010-11-13 08:09:54 +0000

    CONO: Version 1.40

--- culfw/version.h
+++ culfw/version.h
@@ -1,3 +1,3 @@
 #define VERSION_1               1
-#define VERSION_2               39
-#define VERSION                 "1.39"
+#define VERSION_2               40
+#define VERSION                 "1.40"
