481301453fa24511c535e7390ae3972a3c2fd696
Bjoern Hempel <info@hempel-online.de>
2011-01-27 11:02:56 +0000

    no message

--- culfw/version.h
+++ culfw/version.h
@@ -1,3 +1,3 @@
 #define VERSION_1               1
-#define VERSION_2               40
-#define VERSION                 "1.40"
+#define VERSION_2               41
+#define VERSION                 "1.41"
