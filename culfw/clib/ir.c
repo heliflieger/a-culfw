c90b8df57a65c0e0e2f7a25d1b57809b6b31073b
Bjoern Hempel <info@hempel-online.de>
2012-01-26 00:14:26 +0000

    Corrected support for sending IR-Commands

--- culfw/clib/ir.c
+++ culfw/clib/ir.c
@@ -44,13 +44,13 @@ void ir_task( void ) {
   IRMP_DATA irmp_data;
 
   if (!ir_mode)
-      return;
+	  return;
      
   if (!irmp_get_data(&irmp_data))
-        return;
+		return;
      
   if ((ir_mode == 2) && (irmp_data.flags & IRMP_FLAG_REPETITION))
-      return;
+	  return;
      
      
   LED_ON();
@@ -65,52 +65,54 @@ void ir_task( void ) {
 
 void ir_sample( void ) {
     if (!ir_mode)
-        return;
+	    return;
 
     (void) irmp_ISR();  // call irmp ISR
 }
 
 uint8_t ir_send_data (void) {
-    return irsnd_ISR();
+	return irsnd_ISR();
 }
 
 void ir_func(char *in) {
-     IRMP_DATA irmp_data;
-     uint8_t higha = 0, lowa = 0, highc = 0, lowc = 0, protocol = 0, flags  = 0;
-     uint16_t address = 0, command = 0;
-     
+#ifdef HAS_IRTX 
+   	 IRMP_DATA irmp_data;
+   	 uint8_t higha = 0, lowa = 0, highc = 0, lowc = 0, protocol = 0, flags  = 0;
+   	 uint16_t address = 0, command = 0;
+#endif   	 
+   	 
      switch (in[1]) {
     #ifdef HAS_IRRX
-        case 'r': // receive
-              if (in[2] && in[3]) {
-             fromhex(in+2, &ir_mode, 2);
-            }
-            DH(ir_mode,2);
-            DNL();
-        break;
-    #endif    
+	    case 'r': // receive
+			  if (in[2] && in[3]) {
+	      	 fromhex(in+2, &ir_mode, 2);
+		  	}
+		  	DH(ir_mode,2);
+		  	DNL();
+	  	break;
+    #endif	  
 
     #ifdef HAS_IRTX 
-        case 's': // send
-          fromhex(in+2, &protocol, 1);
-          fromhex(in+4, &higha, 1);
-          fromhex(in+6, &lowa, 1);
-          address = higha*256+lowa;
-          fromhex(in+8, &highc, 1);
-          fromhex(in+10, &lowc, 1);
-          command = highc*256+lowc;
-          fromhex(in+12, &flags, 1);
+    	case 's': // send
+	      fromhex(in+2, &protocol, 1);
+	      fromhex(in+4, &higha, 1);
+	      fromhex(in+6, &lowa, 1);
+	      address = higha*256+lowa;
+	      fromhex(in+8, &highc, 1);
+	      fromhex(in+10, &lowc, 1);
+	      command = highc*256+lowc;
+	      fromhex(in+12, &flags, 1);
 
         irmp_data.protocol = protocol;
         irmp_data.address  = address;
         irmp_data.command  = command;
         irmp_data.flags    = flags;
 
-          irsnd_send_data (&irmp_data, TRUE);
+	      irsnd_send_data (&irmp_data, TRUE);
 
-        break;
-    #endif    
-            
+	  	break;
+    #endif	  
+			
     #ifdef HAS_IR_POWER
       case 'i': // Internal Reciever on/off
         if (ir_internal) {
@@ -121,10 +123,10 @@ void ir_func(char *in) {
           ir_internal = 1;
         }
         DH(ir_internal,2);
-        DNL();          
-        break;
+  	    DNL();  		
+	  	break;
     #endif
-      
+	  
     }
 
 }
