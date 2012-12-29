2a74d9f3060424abaf9eedccedd5067f8ab1339b
Bjoern Hempel <info@hempel-online.de>
2012-12-29 11:33:59 +0000

    Bump version to 1.50

--- culfw/version.h
+++ culfw/version.h
@@ -1,3 +1,3 @@
 #define VERSION_1               1
-#define VERSION_2               49
-#define VERSION                 "1.49"
+#define VERSION_2               50
+#define VERSION                 "1.50"
