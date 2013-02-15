0e6170c9e7158f77fd8a0542d3845b710cbb7126
Bjoern Hempel <info@hempel-online.de>
2013-02-15 03:20:57 +0000

    Bump version to 1.53

--- culfw/version.h
+++ culfw/version.h
@@ -1,3 +1,3 @@
 #define VERSION_1               1
-#define VERSION_2               52
-#define VERSION                 "1.52"
+#define VERSION_2               53
+#define VERSION                 "1.53"
