64580db54e15266b8d020138521d3f680a1f563f
Bjoern Hempel <info@hempel-online.de>
2013-06-28 23:12:45 +0000

    add support for RFbee-hardware

---
+++ culfw/Devices/RFbee/RFbee.c
@@ -0,0 +1,158 @@
+/* Copyright Rudolf Koenig, 2008.
+   Released under the GPL Licence, Version 2
+   Inpired by the MyUSB USBtoSerial demo, Copyright (C) Dean Camera, 2008.
+*/
+
+#include <avr/boot.h>
+#include <avr/power.h>
+#include <avr/eeprom.h>
+#include <avr/interrupt.h>
+#include <avr/io.h>
+#include <avr/pgmspace.h>
+#include <avr/wdt.h>
+
+#include <string.h>
+
+#include "board.h"
+
+#include "spi.h"
+#include "cc1100.h"
+#include "clock.h"
+#include "delay.h"
+#include "display.h"
+#include "serial.h"
+#include "fncollection.h"
+#include "led.h"
+#include "ringbuffer.h"
+#include "rf_receive.h"
+#include "rf_send.h"
+#include "ttydata.h"
+#include "fht.h"
+#include "fastrf.h"
+#include "rf_router.h"
+#include "memory.h"
+
+#ifdef HAS_MEMFN
+#include "memory.h"
+#endif
+
+#ifdef HAS_INTERTECHNO
+#include "intertechno.h"
+#endif
+
+#ifdef HAS_ASKSIN
+#include "rf_asksin.h"
+#endif
+
+#ifdef HAS_MORITZ
+#include "rf_moritz.h"
+#endif
+
+#ifdef HAS_RWE
+#include "rf_rwe.h"
+#endif
+
+const PROGMEM t_fntab fntab[] = {
+  { 'B', prepare_boot },
+  { 'C', ccreg },
+  { 'F', fs20send },
+#ifdef HAS_INTERTECHNO
+  { 'i', it_func },
+#endif
+#ifdef HAS_ASKSIN
+  { 'A', asksin_func },
+#endif
+#ifdef HAS_MORITZ
+  { 'Z', moritz_func },
+#endif
+#ifdef HAS_RWE
+  { 'E', rwe_func },
+#endif
+#ifdef HAS_RAWSEND
+  { 'G', rawsend },
+  { 'M', em_send },
+#endif
+  { 'R', read_eeprom },
+  { 'T', fhtsend },
+  { 'V', version },
+  { 'W', write_eeprom },
+  { 'X', set_txreport },
+
+  { 'e', eeprom_factory_reset },
+#ifdef HAS_FASTRF
+  { 'f', fastrf_func },
+#endif
+#ifdef HAS_MEMFN
+  { 'm', getfreemem },
+#endif
+  { 't', gettime },
+#ifdef HAS_RF_ROUTER
+  { 'u', rf_router_func },
+#endif
+  { 'x', ccsetpa },
+
+  { 0, 0 },
+};
+
+int
+main(void)
+{
+  wdt_disable();
+
+  led_init();
+
+  spi_init();
+
+//  eeprom_factory_reset("xx");
+  eeprom_init();
+
+  // Setup the timers. Are needed for watchdog-reset
+  OCR0A  = 249;                            // Timer0: 0.008s = 8MHz/256/250 == 125Hz
+  TCCR0B = _BV(CS02);
+  TCCR0A = _BV(WGM01);
+  TIMSK0 = _BV(OCIE0A);
+
+  TCCR1A = 0;
+  TCCR1B = _BV(CS11) | _BV(WGM12);         // Timer1: 1us = 8MHz/8
+
+  clock_prescale_set(clock_div_1);
+
+  MCUSR &= ~(1 << WDRF);                   // Enable the watchdog
+  wdt_enable(WDTO_2S);
+
+  uart_init( UART_BAUD_SELECT(UART_BAUD_RATE,F_CPU) );
+
+  fht_init();
+  tx_init();
+  input_handle_func = analyze_ttydata;
+
+  display_channel = DISPLAY_USB;
+
+#ifdef HAS_RF_ROUTER
+  rf_router_init();
+  display_channel |= DISPLAY_RFROUTER;
+#endif
+
+  sei();
+
+  for(;;) {
+    uart_task();
+    RfAnalyze_Task();
+    Minute_Task();
+#ifdef HAS_FASTRF
+    FastRF_Task();
+#endif
+#ifdef HAS_RF_ROUTER
+    rf_router_task();
+#endif
+#ifdef HAS_ASKSIN
+    rf_asksin_task();
+#endif
+#ifdef HAS_MORITZ
+    rf_moritz_task();
+#endif
+#ifdef HAS_RWE
+    rf_rwe_task();
+#endif
+  }
+}
---
+++ culfw/Devices/RFbee/board.h
@@ -0,0 +1,61 @@
+#ifndef _BOARD_H
+#define _BOARD_H
+
+#define HAS_FHT_8v			// PROGMEM:  434b, RAM: 19b
+#define HAS_FHT_80b			// PROGMEM: 1158b, RAM:  5b
+#define FHTBUF_SIZE		174	//                 RAM: 174b
+#define RCV_BUCKETS		4	//                 RAM: 25b * bucket
+#undef  RFR_DEBUG			// PROGMEM:  354b  RAM: 14b
+#define FULL_CC1100_PA			// PROGMEM:  108b
+#define HAS_RAWSEND			//
+#define HAS_FASTRF			// PROGMEM:  468b  RAM:  1b
+#define HAS_ASKSIN
+#undef  HAS_ESA
+#define HAS_INTERTECHNO
+#define HAS_MORITZ
+#undef  HAS_RWE
+#define HAS_TX3
+#define HAS_HOERMANN
+#undef  HAS_MEMFN
+#define HAS_RF_ROUTER
+
+/*
+ * Board definition according to
+ * http://www.seeedstudio.com/depot/datasheet/RFBee%20User%20Manual%20v1.1.pdf
+ */
+
+#define SPI_PORT		PORTB
+#define SPI_DDR			DDRB
+#define SPI_SS			2
+#define SPI_MISO		4
+#define SPI_MOSI		3
+#define SPI_SCLK		5
+
+#define CC1100_CS_DDR		SPI_DDR
+#define CC1100_CS_PORT		SPI_PORT
+#define CC1100_CS_PIN		SPI_SS
+
+#define CC1100_OUT_DDR		DDRD
+#define CC1100_OUT_PORT		PORTD
+#define CC1100_OUT_PIN		2
+
+#define CC1100_IN_DDR		DDRD
+#define CC1100_IN_PORT		PIND
+#define CC1100_IN_PIN		3
+#define CC1100_INT		INT1
+#define CC1100_INTVECT		INT1_vect
+#define CC1100_ISC		ISC10
+#define CC1100_EICR		EICRA
+
+#define LED_DDR			DDRD
+#define LED_PORT		PORTD
+#define LED_PIN			6
+
+#define BOARD_ID_STR		"RFbee"
+
+#define HAS_UART		1
+#define UART_BAUD_RATE		38400
+
+#define TTY_BUFSIZE		64
+
+#endif
