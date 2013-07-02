c91c645347e38a8330b04642115d74d042530278
Bjoern Hempel <info@hempel-online.de>
2013-07-02 19:15:00 +0000

    Updated Version to 1.57

--- culfw/version.h
+++ culfw/version.h
@@ -1,3 +1,3 @@
 #define VERSION_1               1
-#define VERSION_2               56
-#define VERSION                 "1.56"
+#define VERSION_2               57
+#define VERSION                 "1.57"
