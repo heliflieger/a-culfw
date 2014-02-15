0b160b40e4b68d6541e1d7a1506ab317db939dee
Bjoern Hempel <info@hempel-online.de>
2014-02-15 14:16:40 +0000

    enable asksin firmware-update-mode for CULv3 and COC

--- culfw/Devices/CUL/board.h
+++ culfw/Devices/CUL/board.h
@@ -25,6 +25,7 @@
 #  define HAS_RAWSEND                   //
 #  define HAS_FASTRF                    // PROGMEM:  468b  RAM:  1b
 #  define HAS_ASKSIN
+#  define HAS_ASKSIN_FUP
 #  define HAS_MORITZ
 #  define HAS_RWE
 #  define HAS_ESA
