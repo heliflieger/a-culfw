f3c009f5c9fa031ea1bebbafccbcf1e5c279a6b8
Bjoern Hempel <bhempel.0705@gmail.com>
2015-01-30 10:47:56 +0100

    dded support for new CSM module (Radino)

---
+++ culfw/Devices/CSM_RADINO/RADINO.c
@@ -0,0 +1,202 @@
+/* Copyright Rudolf Koenig, 2008.
+   Released under the GPL Licence, Version 2
+   Inpired by the MyUSB USBtoSerial demo, Copyright (C) Dean Camera, 2008.
+*/
+
+#include <avr/boot.h>
+#include <avr/power.h>
+#include <avr/eeprom.h>
+#include <avr/interrupt.h>
+#include <avr/io.h>
+#include <avr/pgmspace.h>
+#include <avr/wdt.h>
+
+#include <string.h>
+
+#include "board.h"
+
+#include "fband.h"
+#include "spi.h"
+#include "cc1100.h"
+#include "clock.h"
+#include "delay.h"
+#include "display.h"
+#include "serial.h"
+#include "fncollection.h"
+#include "led.h"
+#include "ringbuffer.h"
+#include "rf_receive.h"
+#include "rf_send.h"
+#include "ttydata.h"
+#include "fht.h"
+#include "fastrf.h"
+#include "rf_router.h"
+#include "memory.h"
+
+#ifdef HAS_INTERTECHNO
+#include "intertechno.h"
+#endif
+
+#ifdef HAS_ASKSIN
+#include "rf_asksin.h"
+#endif
+
+#ifdef HAS_MORITZ
+#include "rf_moritz.h"
+#endif
+
+#ifdef HAS_STACKING
+#include "stacking.h"
+#endif
+
+#ifdef HAS_SOMFY_RTS
+#include "somfy_rts.h"
+#endif
+
+#ifdef HAS_MBUS
+#include "rf_mbus.h"
+#endif
+
+const PROGMEM t_fntab fntab[] = {
+
+  { 'm', getfreemem },
+
+  { 'B', prepare_boot },
+#ifdef HAS_MBUS
+  { 'b', rf_mbus_func },
+#endif
+  { 'C', ccreg },
+  { 'F', fs20send },
+#ifdef HAS_INTERTECHNO
+  { 'i', it_func },
+#endif
+#ifdef HAS_ASKSIN
+  { 'A', asksin_func },
+#endif
+#ifdef HAS_MORITZ
+  { 'Z', moritz_func },
+#endif
+#ifdef HAS_RAWSEND
+  { 'G', rawsend },
+  { 'M', em_send },
+//  { 'S', esa_send },
+#endif
+#ifdef HAS_SOMFY_RTS
+  { 'Y', somfy_rts_func },
+#endif
+  { 'R', read_eeprom },
+  { 'T', fhtsend },
+  { 'V', version },
+  { 'W', write_eeprom },
+  { 'X', set_txreport },
+
+  { 'e', eeprom_factory_reset },
+#ifdef HAS_FASTRF
+  { 'f', fastrf_func },
+#endif
+#ifdef HAS_STACKING
+  { '*', stacking_func },
+#endif
+  { 'l', ledfunc },
+  { 't', gettime },
+#ifdef HAS_RF_ROUTER
+  { 'u', rf_router_func },
+#endif
+  { 'x', ccsetpa },
+
+  { 0, 0 },
+};
+
+
+void
+start_bootloader(void)
+{
+  cli();
+
+  /* move interrupt vectors to bootloader section and jump to bootloader */
+  MCUCR = _BV(IVCE);
+  MCUCR = _BV(IVSEL);
+
+#define jump_to_bootloader ((void(*)(void))0x1800)
+  jump_to_bootloader();
+}
+
+int
+main(void)
+{
+  wdt_disable();
+
+  led_init();
+  LED_ON();
+
+  MARK433_PORT |= _BV( MARK433_BIT );
+
+  spi_init();
+
+//  eeprom_factory_reset("xx");
+  eeprom_init();
+
+//  led_mode = 2;
+
+  // if we had been restarted by watchdog check the REQ BootLoader byte in the
+  // EEPROM ...
+//  if(bit_is_set(MCUSR,WDRF) && eeprom_read_byte(EE_REQBL)) {
+//    eeprom_write_byte( EE_REQBL, 0 ); // clear flag
+//    start_bootloader();
+//  }
+
+  // Setup the timers. Are needed for watchdog-reset
+  OCR0A  = 249;                            // Timer0: 0.008s = 8MHz/256/250 == 125Hz
+  TCCR0B = _BV(CS02);
+  TCCR0A = _BV(WGM01);
+  TIMSK0 = _BV(OCIE0A);
+
+  TCCR1A = 0;
+  TCCR1B = _BV(CS11) | _BV(WGM12);         // Timer1: 1us = 8MHz/8
+
+  clock_prescale_set(clock_div_1);
+
+  MCUSR &= ~(1 << WDRF);                   // Enable the watchdog
+  wdt_enable(WDTO_2S);
+
+  uart_init( UART_BAUD_SELECT_DOUBLE_SPEED(UART_BAUD_RATE,F_CPU) );
+
+  fht_init();
+  tx_init();
+  input_handle_func = analyze_ttydata;
+
+  display_channel = DISPLAY_USB;
+
+#ifdef HAS_RF_ROUTER
+  rf_router_init();
+  display_channel |= DISPLAY_RFROUTER;
+#endif
+
+  checkFrequency(); 
+  LED_OFF();
+
+  sei();
+
+  for(;;) {
+    uart_task();
+    RfAnalyze_Task();
+    Minute_Task();
+#ifdef HAS_FASTRF
+    FastRF_Task();
+#endif
+#ifdef HAS_RF_ROUTER
+    rf_router_task();
+#endif
+#ifdef HAS_ASKSIN
+    rf_asksin_task();
+#endif
+#ifdef HAS_MORITZ
+    rf_moritz_task();
+#endif
+#ifdef HAS_MBUS
+    rf_mbus_task();
+#endif
+
+  }
+
+}
---
+++ culfw/Devices/CSM_RADINO/board.h
@@ -0,0 +1,107 @@
+#ifndef _BOARD_H
+#define _BOARD_H
+
+#define HAS_FHT_8v                      // PROGMEM:  434b, RAM: 19b
+#define HAS_FHT_80b                     // PROGMEM: 1158b, RAM:  5b
+#define HAS_FHT_TF
+
+#undef  FULL_CC1100_PA                  // PROGMEM:  108b
+
+#undef  HAS_RAWSEND                     // PROGMEM:  198b     RAM:  4b
+#undef  HAS_FASTRF                      // PROGMEM:  362+106  RAM:  1b
+#undef  HAS_RF_ROUTER                   // PROGMEM:  920b  RAM: 38b
+#undef  HAS_LONGMSG                     // CUR support     RAM: 20b
+
+#define SPI_PORT		PORTB
+#define SPI_DDR			DDRB
+#define SPI_IN			PINB
+#define SPI_SS			0
+#define SPI_MISO		3
+#define SPI_MOSI		2
+#define SPI_SCLK		1
+
+#define CC1100_CS_DDR		DDRB
+#define CC1100_CS_PORT          PORTB
+#define CC1100_CS_PIN		4
+
+#define CC1100_OUT_DDR          DDRB
+#define CC1100_OUT_PORT         PORTB
+#define CC1100_OUT_PIN          5
+#define CC1100_OUT_IN           PINB
+
+#define CC1100_IN_DDR	        DDRE
+#define CC1100_IN_PORT          PINE
+#define CC1100_IN_PIN           6
+#define CC1100_IN_IN            PINE
+#define CC1100_INT	        INT6
+#define CC1100_INTVECT          INT6_vect
+#define CC1100_ISC	        ISC60
+#define CC1100_EICR             EICRB
+#define LED_DDR                 DDRB
+#define LED_PORT                PORTB
+#define LED_PIN                 0
+
+#define HAS_UART                1
+#define UART_BAUD_RATE          38400
+#define USART_RX_vect           USART1_RX_vect
+#define USART_UDRE_vect         USART1_UDRE_vect
+
+// we map uart 1 => uart 0 here (which is not existing)
+#define UDR0 UDR1
+#define UCSR0B UCSR1B
+#define UDRIE0 UDRIE1
+#define UCSR0A UCSR1A
+#define FE0 FE1
+#define DOR0 DOR1
+#define U2X0 U2X1
+#define UBRR0 UBRR1
+#define RXCIE0 RXCIE1
+#define RXEN0 RXEN1
+#define TXEN0 TXEN1
+#define UCSR0C UCSR1C
+#define UCSZ00 UCSZ10
+#define UCSZ01 UCSZ11
+#define UDRIE0 UDRIE1
+
+#define FHTBUF_SIZE          174      //                 RAM: 174b
+#define RCV_BUCKETS            4      //                 RAM: 25b * bucket
+#define RFR_DEBUG                     // PROGMEM:  354b  RAM: 14b
+#define FULL_CC1100_PA                // PROGMEM:  108b
+#define HAS_RAWSEND                   //
+#define HAS_FASTRF                    // PROGMEM:  468b  RAM:  1b
+#define HAS_RF_ROUTER                 // PROGMEM:  920b  RAM: 38b
+#define HAS_ASKSIN
+#define HAS_MORITZ
+#define HAS_ESA
+#define HAS_INTERTECHNO
+#define HAS_SOMFY_RTS
+#define HAS_MBUS
+#define HAS_CC1101_RX_PLL_LOCK_CHECK_TASK_WAIT
+#define HAS_CC1101_PLL_LOCK_CHECK_MSG
+#define HAS_CC1101_PLL_LOCK_CHECK_MSG_SW
+#define HAS_ASKSIN_FUP
+#define HAS_RWE
+#define HAS_TX3
+#define HAS_UNIROLL
+#define HAS_HOERMANN
+#define HAS_IT
+#define HAS_TCM97001
+
+#define MULTI_FREQ_DEVICE       // available in multiple versions: 433MHz,868MHz,915MHz
+
+#define MARK433_PORT            PORTD
+#define MARK433_PIN             PIND
+#define MARK433_BIT             4
+
+#define BOARD_ID_STR            "CSM868"
+#define BOARD_ID_STR433         "CSM433"
+#define BOARD_ID_STR915         "CSM915"
+
+#define HAS_UART                1
+#define UART_BAUD_RATE          38400
+
+#define TTY_BUFSIZE             128
+
+#define BUSWARE_CSM
+
+#endif
