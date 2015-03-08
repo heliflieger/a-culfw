a498524a80244fcea4cc2ddaf71a98bb28b8630c
Bjoern Hempel <bhempel.0705@gmail.com>
2015-03-08 10:38:40 +0100

    Rename culfw to a-culfw Set Version to 1.00.00

--- culfw/version.h
+++ culfw/version.h
@@ -1,6 +1,6 @@
 #define VERSION_1               1
-#define VERSION_2               63
-#define VERSION                 "1.63.02"
+#define VERSION_2               0
+#define VERSION                 "1.00.00"
 #define BUILD_DATE              "unknown"
 #define BUILD_NUMBER            "private build"
-#define FW_NAME                 "culfw"
+#define FW_NAME                 "a-culfw"
