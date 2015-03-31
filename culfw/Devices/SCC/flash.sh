1c83f58bf6884afeac7b815a06193b2c115bd2a5
Bjoern Hempel <bhempel.0705@gmail.com>
2015-03-31 21:41:44 +0200

    Add flash script for SCC

--- culfw/Devices/SCC/flash.sh
+++ culfw/Devices/SCC/flash.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # 
-# This Script flash the COC device
+# This Script flash the SCC device
 #
 # To init call ./flash.sh
 #
 
 # The file for flashing the device
-FLASH_FILE=COC.hex
+FLASH_FILE=SCC.hex
 
 # The MCU
-MCU=atmega32u4
+MCU=atmega1284p
 
 # working dir
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
@@ -26,26 +26,11 @@ if [ ! -e "$FLASH_FILE" ] ; then
   make
 fi 
 
+
 echo "-------------------------------------------------------------"
-echo "This program flash the COC device with new firmware."
+echo "This program flash the cul device with new firmware."
 echo "Please change the device into the bootloader"
 echo "-------------------------------------------------------------"
-echo "Please choose a device:"
-echo " 1 = COC"
-echo " 2 = COC_radio_only"
-read -p "Please select device (1-2): " device
-if [ "X$device" == "X" -o "$device" != "1" -a "$device" != "2" ] ; then
-   echo "Abort"
-   exit
-fi
-if [ "$device" == "1" ] ; then
-  MCU=atmega1284p
-  FLASH_FILE=COC.hex
-elif [ "$device" == "2" ] ; then
-  MCU=atmega1284p
-  FLASH_FILE=COC_radio_only.hex
-fi
-
 read -p "Please insert the port for your device [default $PORT]: " port
 if [ "X$port" == "X" ] ; then
    port="$PORT"
@@ -57,6 +42,7 @@ fi
 
 echo ""
 echo "The device will now be flashed"
+echo "KEEP THE MICRO BUTTON PRESSED AT DESIRED EXTENSION"
 read -p "Continue (y/n)?" flashdevice
 
 if [ "$flashdevice" == "y" -o "$flashdevice" == "Y" -o "$flashdevice" == "j" -o "$flashdevice" == "J" ] ; then
@@ -80,7 +66,10 @@ if [ "$flashdevice" == "y" -o "$flashdevice" == "Y" -o "$flashdevice" == "j" -o 
   echo "Call now ${PROGRAMMER} -p${MCU} -cavr109 -P${port} -b${BAUD} -D -Uflash:w:./${FLASH_FILE}:i"
   ${PROGRAMMER} -p${MCU} -cavr109 -P${port} -b${BAUD} -D -Uflash:w:./${FLASH_FILE}:i
 
-  if test -e /sys/bus/i2c/devices/0-0050/eeprom; then echo COC V1.1 $(COCVERS) `date +%F` > /sys/bus/i2c/devices/0-0050/eeprom; fi
+  echo 0 > /sys/class/gpio/gpio17/value
+	sleep 1
+	echo 1 > /sys/class/gpio/gpio17/value
+
 else
   echo "Abort flash"
 fi
