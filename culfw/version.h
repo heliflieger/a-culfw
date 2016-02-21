c5fca64ed78ee5325d0ce08b6ff77776764b2e42
Damian Melson <damian.melson@osnanet.de>
2016-02-21 13:24:17 +0100

    Update version.h

--- culfw/version.h
+++ culfw/version.h
@@ -1,6 +1,6 @@
 #define VERSION_1               1
 #define VERSION_2               20
-#define VERSION                 "1.20.05"
+#define VERSION                 "1.20.06"
 #define BUILD_DATE              "unknown"
 #define BUILD_NUMBER            "private build"
 #define FW_NAME                 "a-culfw"
