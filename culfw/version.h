20ff4afcba5f61def5b2c0d1c60b8d5236fe394c
Telekatz <telekatz@gmx.de>
2017-03-29 19:18:34 +0200

    1.24.02 - Add CC113L to hardware autodetection - Fix tcp_putchar - Fix MBUS for multi CC1101 devices

--- culfw/version.h
+++ culfw/version.h
@@ -1,6 +1,6 @@
 #define VERSION_1               1
 #define VERSION_2               24
-#define VERSION                 "1.24.01"
+#define VERSION                 "1.24.02"
 #define BUILD_DATE              "unknown"
 #define BUILD_NUMBER            "private build"
 #define FW_NAME                 "a-culfw"
